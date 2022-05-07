3b13b1a7045e1d88c1656568f30fc1569fd8339d
Ashwitha Banoth <85865002+Ashwitha66@users.noreply.github.com>
2022-05-08 02:50:28 +0530

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,12 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'app_name', 
+]
+
+MEDIA_ROOT = 'D:\\django_projects\\django_file_sharing\\dj_file_sharing\\main\\media'
+MEDIA_URL = 'media/'
